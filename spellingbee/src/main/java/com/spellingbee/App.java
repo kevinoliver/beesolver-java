33d2730889c630042eee27ffb988b73adc007259
Kevin Oliver <kevinoliver@gmail.com>
2023-03-14 16:29:53 -0700

    start making the app work

--- spellingbee/src/main/java/com/spellingbee/App.java
+++ spellingbee/src/main/java/com/spellingbee/App.java
@@ -1,11 +1,18 @@
 package com.spellingbee;
 
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        System.out.println("🐝🐝🐝🐝🐝🐝🐝🐝🐝🐝🐝🐝🐝🐝🐝🐝🐝🐝🐝");
+        System.out.println("Hello and welcome to Spelling Bee Solver");
+        System.out.println("🐝🐝🐝🐝🐝🐝🐝🐝🐝🐝🐝🐝🐝🐝🐝🐝🐝🐝🐝");
+
+        Words words = Words.load();
+        Solver solver = Solver.fromWords(words);
     }
 }
