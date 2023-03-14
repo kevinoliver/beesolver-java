0bc235849f63d6e7f90b050beb8aa0c4e65150fa
Kevin Oliver <kevinoliver@gmail.com>
2023-03-14 15:55:49 -0700

    Start of a words file and class for reading it.
    Using Debain's wamerican-small package as the initial dictionary.

---
+++ spellingbee/src/main/java/com/spellingbee/Words.java
@@ -0,0 +1,37 @@
+package com.spellingbee;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Words {
+
+    private static final String DEFAULT_PATH = "/american-english-small";
+
+    // todo: refactor for testing other dictionaries
+    public static Words load() throws IOException {
+        URI in;
+        try {
+            in = Words.class.getClassLoader().getResource(DEFAULT_PATH).toURI();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        try (Stream<String> lines = Files.lines(Path.of(in))) {
+            // todo: probably not the right data structure 
+            Set<String> words = lines.collect(Collectors.toSet());
+            return new Words(words);
+        }
+    }
+
+    private final Set<String> words;
+
+    private Words(Set<String> words) {
+        this.words = Objects.requireNonNull(words);
+    }
+}
